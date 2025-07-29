d84fa7d7c151ffc410942b9d9417c72b0a4d0687
Daniel Whitley <thisdwhitley@gmail.com>
2025-07-29 15:11:07 -0400

    removing old unused page

--- src/app/demo/stands/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/demo/stands/page.tsx
-import StandCardDemoEnhanced from '@/components/stands/StandCardDemoEnhanced'
-
-export default function StandDemoPage() {
-  return <StandCardDemoEnhanced />
-}
